1f29b8f4c68ba5cbc0a42c0c1faacdcde82b35d0
zz9pzza <zz9pzza@gmail.com>
2016-04-04 23:51:34 +0100

    Add ansible

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,6 +6,9 @@ perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#http://mirror.rackspace.com
 # Update the box
 apt-get -y update >/dev/null
 apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+apt-get -y install python-pip python-dev git >/dev/null
+
+pip install ansible
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
