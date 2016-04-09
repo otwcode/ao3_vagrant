3631db5da01ebc68bfe80107866e86cc9c4ae319
zz9pzza <zz9pzza@gmail.com>
2016-04-09 09:27:55 +0100

    Add an ansible playbook

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -7,11 +7,10 @@ apt-get -y update
 apt-get -y install curl
 
 # You can install anything you need here.
-exit 0
 # Configure mysql
 echo 'mysql-server mysql-server/root_password password root'       | debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password root' | debconf-set-selections
-
+exit
 
 # Install necessary dependencies
 apt-get -y -q install  wget git tmux firefox xvfb vim atop
